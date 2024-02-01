a30b4cef615c92a920d5da4504ce23303e42ed71
Niklas Egger <egger@hdm-stuttgart.de>
2024-02-01 10:41:36 +0100

    update content a11y material pack

--- src/lib/constants/accessibleMaterialPack.constant.ts
+++ src/lib/constants/accessibleMaterialPack.constant.ts
@@ -13,9 +13,11 @@ export const ACCESSIBLE_MATERIAL_PACK: Project = {
 	summary:
 		'Das Materialpaket bündelt zentrale Informationen zur Umsetzung barrierefreier Hochschullehre. Knappe Checklisten geben Hinweise zur Umsetzung möglichst barrierefreier Dokumente und Lehrsituationen. Ergänzend dazu findet man ausführliche Anleitungen und Hilfestellungen zur Umsetzung der in den Checklisten aufgeführten Anforderungen. In einer Wissensdatenbank werden zusätzlich Informationen zu weiteren Aspekten im Kontext barrierefreier digitaler Lehre bereitgestellt.',
 	partners: ['Universität Bielefeld'],
+	info: 'Damit Sie Zugang zu den Inhalten des Kurses erhalten, müssen Sie sich vorab anmelden. Dazu können Sie ein bestehendes Moodle-Konto nutzen oder ein kostenloses Konto anlegen. Nach dem erfolgreichen Login haben Sie Zugriff auf den Kurs.',
 	joinEvaluation: {
-		title: 'Projektpartner*in werden',
-		introduction: '',
+		title: 'Feedback geben',
+		introduction:
+			'Unser umfangreiches Materialpaket ist ab sofort mit einem Gastzugang frei zugänglich. Nutzen Sie das Forum im Kurs oder die angegebene Kontaktadresse für Feedback, Diskussionen und Ideenaustausch. Ihre Rückmeldungen helfen uns dabei, unsere Materialien stetig zu verbessern.',
 		contactName: 'Judith Kuhlmann',
 		contactEmail: 'jkuhlmann3@uni-bielefeld.de'
 	},
