219c4ff6bcfa2c240efd24314cc268ec77da6cdb
Niklas Egger <egger@hdm-stuttgart.de>
2024-02-15 14:39:36 +0100

    fix typos

--- src/lib/constants/iliasCourse.constant.ts
+++ src/lib/constants/iliasCourse.constant.ts
@@ -8,16 +8,16 @@ export const ILIAS_COURSE: Project = {
 		'Ein Lernraum der Informationen, Beispiele und Ressourcen zum Thema (digitale) Barrierefreiheit in und um ILIAS bietet.',
 	targetGroup: ['Für Lehrende'],
 	summary:
-		'Sowohl auf ILIAS selbst als auch bei der Gestaltung von Lehrmaterialien mit Drittanwendungen (wie z.B. den Office-Programmen Word etc.), können unabsichtlich Barrieren eingebaut werden, die einige Nutzer*innen ausschließen. In diesem Kurs lernen Sie, worauf Sie achten müssen, um solche Barrieren zu vermeiden. Außerdem finden Sie hier Hintergrundinformationen zu Begriffen wie Inklusion und verschiedenen Arten von Beeinträchtigungen sowie weiterführende Links.',
+		'Sowohl auf ILIAS selbst, als auch bei der Gestaltung von Lehrmaterialien mit Drittanwendungen (wie z.B. den Office-Programmen Word etc.), können unabsichtlich Barrieren eingebaut werden, die einige Nutzer*innen ausschließen. In diesem Kurs lernen Sie, worauf Sie achten müssen, um solche Barrieren zu vermeiden. Außerdem finden Sie hier Hintergrundinformationen zu Begriffen wie Inklusion und verschiedenen Arten von Beeinträchtigungen sowie weiterführende Links.',
 	partners: ['Pädagogische Hochschule Freiburg'],
 	link: {
-		label: 'Zum Lernraum für (digitale) Barrierefreiheit in der Lehre (Ilias)',
+		label: 'Zum Lernraum für (digitale) Barrierefreiheit in der Lehre (ILIAS)',
 		toProject: 'https://ilias-extern.ph-freiburg.de/goto.php?target=cat_18205&client_id=phfr-ext'
 	},
 	joinEvaluation: {
 		title: 'Feedback und Rückfragen',
 		introduction:
-			'Damit wir unser Angebot für Sie optimieren können, sind wir auf Ihr Feedback angewiesen. Haben Sie Fragen zu den Inhalten, Einwände oder Anregungen, schreiben Sie uns eine E-Mail. Wir freuen uns über jede Nachricht.',
+			'Damit wir unser Angebot für Sie optimieren können, sind wir auf Ihr Feedback angewiesen. Haben Sie Fragen zu den Inhalten, Einwände, oder Anregungen, schreiben Sie uns eine E-Mail. Wir freuen uns über jede Nachricht.',
 		contactEmail: 'shuffle@ph-freiburg.de',
 		contactName: 'dem SHUFFLE Freiburg Team'
 	}
