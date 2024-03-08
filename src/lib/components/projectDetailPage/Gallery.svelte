e2d0368139ebca0682ddd4ae8bc5de15abb7126d
Niklas Egger <egger@hdm-stuttgart.de>
2024-03-08 13:24:53 +0100

    add maturity model images

--- src/lib/components/projectDetailPage/Gallery.svelte
+++ src/lib/components/projectDetailPage/Gallery.svelte
@@ -4,42 +4,20 @@
 
 	export let gallery: Image[];
 	export let projectId: string;
-
-	// let randomId = (Math.random() + 1).toString(36).substring(7);
-
-	// let picture = `${base}/static/gallery/${projectId}/blinddate-gallery-3.png`;
-	// console.log(picture);
 </script>
 
 <div class="wrapper">
-	<!-- <enhanced:img src="/static/gallery/blinddate/blinddate-gallery-3.png" alt="" /> -->
-
-	<!-- {#if gallery && projectId}
-		{#each gallery as image, i}
-			<enhanced:img src="{base}/gallery/{projectId}/{image.src}" alt={image.alt ?? ''} />
-
-			{#if image.visibleDescription}
-				<p id="gallery-img-">{image.visibleDescription}</p>
-			{/if}
-		{/each}
-	{/if} -->
-
 	<h2>Bildergallerie</h2>
 	{#each gallery as image, i}
-		<div class="image-wrapper">
-			<div class="image">
-				<img
-					id="img-{projectId}-{i}"
-					src="{base}/gallery/{projectId}/{image.src}"
-					alt=""
-					aria-hidden="true"
-				/>
+		<figure class="image-wrapper">
+			<div class="image" role="group">
+				<img id="img-{projectId}-{i}" src="{base}/gallery/{projectId}/{image.src}" alt="" />
 			</div>
 			<div class="line" />
-			<div class="description">
+			<figcaption class="description">
 				<p>{image.description}</p>
-			</div>
-		</div>
+			</figcaption>
+		</figure>
 	{/each}
 </div>
 
@@ -68,7 +46,6 @@
 				min-width: 65%;
 				img {
 					object-fit: contain;
-
 					max-width: 100%;
 				}
 			}
