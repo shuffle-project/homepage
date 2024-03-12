31d1662694018327b6fb8ff787582dd78430f67d
Niklas Egger <egger@hdm-stuttgart.de>
2024-03-12 15:43:31 +0100

    move to accessible carousel

--- src/lib/components/mainPage/ProjectsMobile.svelte
+++ src/lib/components/mainPage/ProjectsMobile.svelte
@@ -1,72 +1,77 @@
 <script lang="ts">
 	import type { Project } from '$lib/interfaces/project.interface';
-	import { type EmblaCarouselType, type EmblaOptionsType } from 'embla-carousel';
-	import emblaCarouselSvelte from 'embla-carousel-svelte';
+	import { Splide, SplideSlide, SplideTrack } from '@splidejs/svelte-splide';
+	import '@splidejs/svelte-splide/css/core';
+
 	import Icon from '../Icon.svelte';
-	import ResultCard from '../ProjectCard.svelte';
+	import ProjectCard from '../ProjectCard.svelte';
 
 	export let selectedProjects: Project[] = [];
 
-	const options: EmblaOptionsType = { loop: true, align: 'center' };
-	let emblaAPI: EmblaCarouselType;
-	let emblaSelectedIndex: number = 0;
+	let carousel: Splide;
+
+	const splideOptions = {
+		type: 'loop',
+		live: false,
+		keyboard: false,
+		i18n: {
+			prev: 'Vorheriges Projekt',
+			next: 'Nächstes Projekt',
+			slide: 'Projekt'
+		},
+		pagination: false,
+		arrows: false
+	};
+
+	let carouselSelectedIndex: number = 0;
 
 	let componentHasFocus = false;
 	$: ariaLiveText =
-		selectedProjects[emblaSelectedIndex].title +
+		selectedProjects[carouselSelectedIndex].title +
 		', ' +
-		(emblaSelectedIndex + 1) +
+		(carouselSelectedIndex + 1) +
 		' von ' +
 		selectedProjects.length;
 
-	const onInit = (e: any) => {
-		emblaAPI = e.detail;
-		emblaAPI.on('select', () => {
-			emblaSelectedIndex = emblaAPI.internalEngine().index.get();
-		});
-	};
-
-	function scrollPrevious() {
-		if (!emblaAPI) return;
-		componentHasFocus = true;
-		emblaAPI.scrollPrev();
-	}
-
-	function scrollNext() {
-		if (!emblaAPI) return;
-		componentHasFocus = true;
-		emblaAPI.scrollNext();
+	function moveSlide(direction: string) {
+		if (!carousel) return;
+		carousel.go(direction);
+		carouselSelectedIndex = carousel.splide.index;
 	}
 </script>
 
-<div aria-roledescription="Karussell" role="navigation" aria-label="Projekte">
+<Splide
+	aria-roledescription="Karussell"
+	role="navigation"
+	aria-label="Projekte"
+	options={splideOptions}
+	bind:this={carousel}
+	class="carousel"
+	hasTrack={false}
+>
 	<button
-		on:click={() => scrollPrevious()}
 		on:focusout={() => (componentHasFocus = false)}
+		on:focusin={() => (componentHasFocus = true)}
+		on:click={() => moveSlide('<')}
 		class="previous-button before-card"
 		aria-label="Vorheriges Projekt"
 	>
 		<Icon svg="arrow-back" size="30" />
 	</button>
-	<div class="embla" use:emblaCarouselSvelte={{ options }} on:emblaInit={onInit}>
-		<div class="embla__container">
-			{#each selectedProjects as project, i (project.id)}
-				<div
-					id="result-card-{i + 1}"
-					class="embla__slide"
-					aria-roledescription="Folie"
-					inert={i !== emblaSelectedIndex}
-				>
-					<ResultCard {project} horizontalCenter />
-				</div>
-			{/each}
-		</div>
-	</div>
+
+	<SplideTrack>
+		{#each selectedProjects as project, i (project.id)}
+			<SplideSlide id="result-card-{i + 1}" aria-roledescription="Folie">
+				<ProjectCard {project} horizontalCenter />
+			</SplideSlide>
+		{/each}
+	</SplideTrack>
+	<div class="splide__arrows" />
 
 	<div class="slider-navigation">
 		<button
-			on:click={() => scrollPrevious()}
 			on:focusout={() => (componentHasFocus = false)}
+			on:focusin={() => (componentHasFocus = true)}
 			class="previous-button after-card"
 			aria-label="Vorheriges Projekt"
 		>
@@ -74,43 +79,46 @@
 		</button>
 
 		<p class="slide-number-info">
-			{emblaSelectedIndex + 1} von {selectedProjects.length}
+			{carouselSelectedIndex + 1} von {selectedProjects.length}
 		</p>
 
 		<button
-			on:click={() => scrollNext()}
 			on:focusout={() => (componentHasFocus = false)}
+			on:focusin={() => (componentHasFocus = true)}
+			on:click={() => moveSlide('>')}
 			class="next-button"
 			aria-label="Nächstes Projekt"
 			><Icon svg="arrow-forward" size="30" />
 		</button>
 	</div>
+</Splide>
 
-	{#if componentHasFocus}
-		<div aria-live="polite" class="sr-only">
-			{#key ariaLiveText}
-				{ariaLiveText}
-			{/key}
-		</div>
-	{/if}
-</div>
+{#if componentHasFocus}
+	<div aria-live="polite" class="sr-only">
+		{#key ariaLiveText}
+			{ariaLiveText}
+		{/key}
+	</div>
+{/if}
 
 <style lang="scss">
-	.slider-navigation {
-		max-width: 16.25rem;
-		position: relative;
-		height: 3rem;
-		margin-inline: auto;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		margin-top: 1.25rem;
-	}
+	.carousel {
+		.slider-navigation {
+			max-width: 16.25rem;
+			position: relative;
+			height: 3rem;
+			margin-inline: auto;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			margin-top: 1.25rem;
 
-	.slide-number-info {
-		text-align: center;
-		color: var(--color-dark-grey);
-		font-size: 0.875rem;
+			.slide-number-info {
+				text-align: center;
+				color: var(--color-dark-grey);
+				font-size: 0.875rem;
+			}
+		}
 	}
 
 	.previous-button,
