b9c39f664d900df82be40e9babb0d2a86d3fa980
Niklas Egger <egger@hdm-stuttgart.de>
2024-03-26 10:13:47 +0100

    initial commit with content + new page

---
+++ src/routes/(footerSubpages)/datenschutz-in-leichter-sprache/+page.svelte
@@ -0,0 +1,294 @@
+<script>
+	import TitleSection from '../../../lib/components/TitleSection.svelte';
+</script>
+
+<svelte:head>
+	<title>Datenschutzerklärung in leichter Sprache - SHUFFLE</title>
+</svelte:head>
+
+<TitleSection>
+	<svelte:fragment slot="title">Datenschutz<wbr />erklärung in leichter Sprache</svelte:fragment>
+</TitleSection>
+
+<div class="page-content headings-with-padding">
+	<h2>Einleitung</h2>
+	<p>
+		Das Projekt SHUFFLE ist ein Forschung·sprojekt <br />an der Hoch·schule der Medien (HdM)
+		Stuttgart.
+		<br />
+		Es gibt auch noch weitere Projekt·partner. <br />Es dauert voraussichtlich 3 Jahre (2021 bis
+		2024).
+		<br />
+		Das Projekt wird von der Stiftung Innovation in der Hochschullehre (STiL) gefördert.
+		<br />
+		Ziel des Projekts ist die Verbesserung der digitalen Barrierefreiheit an Hochschulen.
+		<br />Die Website informiert über das Projekt <br /> und erleichtert die Kontaktaufnahme.
+	</p>
+
+	<h2>(Personenbezogene) Daten</h2>
+	<p>
+		Persönliche Daten sind Infos von Ihnen oder über Sie.
+		<br />Diese Daten sind „personen·bezogen“ <br />im Sinne des Daten·schutz-Rechts.
+		<br />Zum Beispiel: Adresse oder Geburts-Datum.
+	</p>
+	<p>
+		<br />Diese Daten werden durch die Daten·schutz-Grund·verordnung geschützt.
+		<br />Die Daten·schutz-Grund·verordnung (DS-GVO) ist ein Gesetz.
+		<br />Die Daten·schutz-Grund·verordnung gilt unabhängig davon,
+		<br />ob Sie unsere Internet-Seite vom Handy besuchen
+		<br /> oder von einem Computer auf die Seite zugreifen.
+	</p>
+
+	<h2>Daten-Verantwortliche</h2>
+	<p>
+		Die Daten·schutz-Grund·verordnung hat mehrere Abschnitte.
+		<br /> Diese Abschnitte heißen dort Artikel (<strong>nicht</strong>: Paragrafen!).
+		<br /> Weil die Artikel Überschriften sind, gibt es noch Absätze.
+	</p>
+	<p>
+		In <a href="https://dejure.org/gesetze/DSGVO/13.html">Artikel 13 Absatz 1 Buchstabe a DS-GVO</a>
+		steht auch, dass
+		<br /> ein Verantwortlicher für den Daten·schutz zu benennen ist.
+	</p>
+	<p>
+		Hier ist das die Hoch·schule der Medien (HdM),
+		<br />Nobelstraße 10,
+		<br />70569 Stuttgart,
+		<br />Projektleitung: Prof. Dr. Gottfried Zimmermann,
+		<br /><a href="mailto:zimmermanng@hdm-stuttgart.de">zimmermanng@hdm-stuttgart.de</a>
+	</p>
+
+	<p>
+		Um Daten zu schützen, gibt es auch eine Daten·schutz-Beauftragte.
+		<br />Sie achtet darauf, dass die personen·bezogenen Daten geschützt werden.
+	</p>
+
+	<p>
+		Bei Fragen zum Daten·schutz können Sie eine E-Mail schreiben an:
+		<br />Lisa Lingner (DSB),
+		<br /> <a href="mailto:datenschutz@hdm-stuttgart.de">datenschutz@hdm-stuttgart.de</a>
+	</p>
+
+	<h2>Umgang mit Daten</h2>
+
+	<p>
+		Diese Internet-Seite arbeitet mit Daten.
+		<br />Das heißt: Daten-Verarbeitung.
+		<br />Die Daten-Verarbeitung auf der Internet-Seite brauchen wir, damit
+	</p>
+
+	<ul>
+		<li>Sie die Seite problemlos sehen und lesen können,</li>
+		<li>wir ein Kontaktformular anbieten und auf Ihre Anfragen antworten können,</li>
+		<li>wir Videos und andere Angebote bereitstellen können.</li>
+	</ul>
+
+	<p>Daten von Personen (personen·bezogene Daten) verarbeiten wir nur</p>
+
+	<ul>
+		<li>wenn wir deren Erlaubnis haben, oder</li>
+		<li>es vom Gesetz erlaubt oder sogar vorgeschrieben ist.</li>
+	</ul>
+
+	<p>
+		Beim Besuch unserer Internet-Seite werden Daten verarbeitet.
+		<br /> Sie haben einen Browser.
+		<br />Zum Beispiel: Internet Explorer oder Firefox.
+		<br />Und wir haben Server.
+		<br />Die meisten davon stehen in der HdM.
+		<br />Ihre Daten werden vom Browser an unsere Server gesendet.
+	</p>
+
+	<p>Dies sind insbesondere:</p>
+
+	<ul>
+		<li>Art und Version des Browsers.</li>
+		<li>Ihr Betriebs-System. Zum Beispiel: Windows.</li>
+		<li>Die angezeigte Internet-Seite.</li>
+		<li>Die Internet-Seite, die Sie vorher besucht haben.</li>
+		<li>Datum und Uhrzeit.</li>
+		<li>IP-Adresse</li>
+	</ul>
+
+	<p>
+		Wenn Sie im Internet sind, machen Sie das mit einer Nummer.
+		<br />Diese Nummer heißt IP-Adresse.
+	</p>
+
+	<h2>Einzelne Verarbeitungen</h2>
+	<h3>Einfache Nutzung der Webseite</h3>
+	<p>Die einfache Nutzung der Website erfordert keine Registrierung.</p>
+	<p>
+		Logfiles (z.B. IP-Adresse, s.o.) werden für den reibungslosen Betrieb der Website gespeichert.
+	</p>
+	<p>
+		Rechtsgrundlage ist das öffentliche Interesse nach <a
+			href="https://dejure.org/gesetze/DSGVO/6.html">Art. 6 Abs. 1 Buchstabe e DS-GVO</a
+		>.
+	</p>
+
+	<h3>Einbindung von Video·inhalten</h3>
+	<p>
+		Videos auf der Website werden meist auf Servern der HdM bereitgestellt.
+		<br />Verlinkte Videos zu YouTube unterliegen jedoch deren Daten·schut·zerklärung.
+	</p>
+
+	<p>
+		Entsprechende Informationen zum Daten·schutz und zu Einstellungen erhalten Sie hier:
+		<br />
+		<a
+			href="
+        https://www.youtube.com/intl/ALL_de/howyoutubeworks/user-settings/privacy/"
+			>YouTube - Datenschutzeinstellungen</a
+		>
+	</p>
+
+	<p>
+		Rechts·grundlage ist die Einwilligung oder ein öffentliches Interesse,
+
+		<br /><a href="https://dejure.org/gesetze/DSGVO/6.html">
+			Art. 6 Abs. 1 Buchstabe a oder Buchstabe e DS-GVO.</a
+		>
+	</p>
+
+	<h3>Zusätzliche Dienste</h3>
+	<p>Kontakt·formulare und E-Mails werden für Anfragen genutzt.</p>
+	<p>Externe Dienst·leister sind vertraglich an DSGVO-konforme Daten·verarbeitung gebunden.</p>
+	<p>
+		Rechts·grundlage ist Einwilligung oder öffentliches Interesse,
+		<br />
+		<a href="https://dejure.org/gesetze/DSGVO/6.html"
+			>Art. 6 Abs. 1 Buchstabe a oder Buchstabe e DS-GVO.</a
+		>
+	</p>
+
+	<h2>Daten-Weitergabe</h2>
+	<p>Personen·bezogene Daten werden grundsätzlich nicht weitergegeben.</p>
+	<p>Gesetzliche Pflichten können im Einzel·fall eine Daten-Weitergabe erfordern.</p>
+	<p>Die Übertragung von Daten zwischen Ihrem Computer und uns ist geschützt.</p>
+
+	<h2>Dauer der Daten-Speicherung</h2>
+	<p>
+		Personen·bezogene Daten werden gelöscht,
+		<br />wenn der Zweck erfüllt ist,
+		<br />
+		und wenn keine gesetzlichen Auf·bewahrungs·pflichten bestehen.
+		<br />
+		Logfiles werden maximal eine Woche gespeichert.
+	</p>
+
+	<h2>Rechte von Betroffenen</h2>
+
+	<p>
+		Betroffene (Nutzende) haben verschiedene Rechte gegenüber dem Verantwortlichen.
+		<br />Diese sind grundsätzlich kostenlos und nötigenfalls auch gerichtlich einklagbar:
+	</p>
+
+	<ul>
+		<li>
+			Recht auf Auskunft über alle eigenen verarbeiteten personen·bezogenen Daten,
+			<a href="https://dejure.org/gesetze/DSGVO/15.html">Art. 15 DS-GVO</a>.
+		</li>
+		<li>
+			Recht auf Berichtigung von fehlerhaften eigenen personen·bezogenen Daten, <a
+				href="https://dejure.org/gesetze/DSGVO/16.html">Art. 16 DS-GVO</a
+			> .
+		</li>
+		<li>
+			Recht auf Löschung von unnötigen eigenen personen·bezogenen Daten, <a
+				href="https://dejure.org/gesetze/DSGVO/17.html">Art. 17 DS-GVO</a
+			> .
+		</li>
+		<li>
+			Recht auf Einschränkung der Verarbeitung von eigenen personen·bezogenen Daten, <a
+				href="https://dejure.org/gesetze/DSGVO/18.html">Art. 18 DS-GVO</a
+			>
+		</li>
+		<li>
+			Bei öffentlichem oder berechtigtem Interesse besteht ein Wider·spruchs·recht, <a
+				href="https://dejure.org/gesetze/DSGVO/21.html">Art. 21 DS-GVO</a
+			> .
+		</li>
+		<li>
+			Einwilligungen können widerrufen werden, <a href="https://dejure.org/gesetze/DSGVO/7.html"
+				>Art. 7 Absatz 3 DS-GVO</a
+			> .
+		</li>
+	</ul>
+
+	<p>
+		Betroffene können ihre Rechte per E-Mail an den Verantwortlichen oder die
+		Daten·schutz·beauftragte (s.o.) geltend machen.
+		<br /> Einzelne Rechte können durch das sog. „Forschungs·privileg“ beschränkt sein,
+		<a href="https://dejure.org/gesetze/BDSG/27.html">§ 27 Absatz 2 BDSG</a>,
+		<a href="https://dejure.org/gesetze/LDSG/13.html">§ 13 Abs. 4 LDSG</a>.
+	</p>
+
+	<p>
+		Darüber hinaus besteht auch ein Beschwerde·recht bei einer Aufsichts·behörde, <a
+			href="https://dejure.org/gesetze/DSGVO/77.html">Art. 77 DS-GVO</a
+		>.
+		<br />Die Beschwerde kann beim Landes·beauftragten für den Daten·schutz und die
+		Informations·freiheit (LfDI) Baden-Württemberg eingelegt werden:
+	</p>
+
+	<p>
+		LfDI Baden-Württemberg
+		<br />Lautenschlagerstraße 20
+		<br />70173 Stuttgart
+	</p>
+
+	<p>
+		Postanschrift:
+		<br /> Postfach 10 29 32
+		<br /> 70025 Stuttgart
+	</p>
+
+	<p>
+		Zentraler Kontakt:
+		<br />Telefon:
+		<br /><a href="tel:07116155410">0711/61 55 41 – 0</a>
+	</p>
+
+	<p>
+		<br />Telefax:
+		<br /><a href="tel:071161554115">0711/61 55 41 – 15</a>
+	</p>
+
+	<p>
+		<br />E-Mail:
+		<br /><a href="mailto:poststelle@lfdi.bwl.de"> poststelle@lfdi.bwl.de </a>
+	</p>
+
+	<p>
+		Die Telefonzentrale des LfDI ist an folgenden Tagen besetzt:
+		<br />Montag und Dienstag: 10-12 Uhr,
+		<br />Mittwoch: 14.00-15.30 Uhr,
+		<br />Donnerstag und Freitag: 10-12 Uhr
+	</p>
+</div>
+
+<style lang="scss">
+	.page-content {
+		max-width: var(--max-text-width);
+		padding-inline: var(--outer-spacing);
+		width: 100%;
+		box-sizing: border-box;
+
+		margin: 1.875rem auto 0 auto;
+	}
+
+	p,
+	li {
+		line-height: 160%;
+	}
+
+	p {
+		margin: 1.275rem 0 0.5rem;
+	}
+
+	ul {
+		padding: 0 0 0 1rem;
+		margin: 0;
+	}
+</style>
