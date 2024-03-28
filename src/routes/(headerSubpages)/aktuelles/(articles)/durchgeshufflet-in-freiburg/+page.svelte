927252c55ca5d1a33d9739da71e15eec05b5f984
Niklas Egger <egger@hdm-stuttgart.de>
2024-03-28 08:07:42 +0100

    new blog entry

---
+++ src/routes/(headerSubpages)/aktuelles/(articles)/durchgeshufflet-in-freiburg/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { base } from '$app/paths';
+	import ArticleHeader from '../../../../../lib/components/ArticleHeader.svelte';
+</script>
+
+<svelte:head>
+	<title>DurchgeSHUFFLEt in Freiburg - SHUFFLE</title>
+</svelte:head>
+
+<ArticleHeader title="DurchgeSHUFFLEt in Freiburg" authorName="Ann-Katrin Böhm" date="28.03.2024" />
+
+<p>
+	Ende März fand unser Frühlingstreffen im sonnigen Freiburg statt. Nachdem wir im Februar die
+	Zusage der Verlängerung des Projekts bis Ende Dezember 2025 bekommen haben, nutzten wir das
+	Präsenztreffen für eine Bestandsaufnahme und Koordination der weiteren Aufgaben. So haben wir uns
+	am Donnerstag zunächst einmal gegenseitig über die aktuellen Projektstände in den einzelnen
+	Teilprojekten informiert. Das hat super geklappt und war sowohl informativ als auch ein lockerer
+	Start in das Präsenztreffen!
+</p>
+
+<p>
+	Am Mittag widmeten wir uns dann der Vorbereitung von Zwischen- und Abschlussberichten, den
+	geplanten Veröffentlichungen und Konferenzteilnahmen und -Beiträgen. Diesen erfolgreichen Tag
+	konnten wir dann mit einem Spaziergang auf den Schlossberg, Führung durch das wunderschöne
+	Freiburg bei sommerlichen Temperaturen und leckerem Abendessen in der „Ersten Liebe“ abschließen.
+</p>
+
+<p>
+	Am Freitag wurden die Überlegungen des Vortages und wichtige Termine direkt festgeklopft. Außerdem
+	konnten wir inhaltlich in den Arbeitspaketen weiterarbeiten. Den Abschluss des Frühlingstreffens
+	bildete dann eine sehr spannende Diskussionsrunde auf der sonnigen Dachterrasse über Ideale und
+	Ansprüche unserer Arbeit.
+</p>
+
+<p>
+	Neben der inhaltlichen Arbeit hatten wir an diesen beiden Tagen mal wieder Zeit, uns persönlich
+	auszutauschen, zu diskutieren und festzustellen, dass wir uns sehr freuen, die nächsten 1 ½ Jahre
+	als Team zusammenzuarbeiten!
+</p>
+
+<img
+	class="max-width-100"
+	src="{base}/articles/durchgeshufflet-in-freiburg/team.jpg"
+	alt="Ein Gruppenfoto des SHUFFLE-Teams auf einer Anhöhe in Freiburg. Im Hintergrund sieht man einen Sonnenuntergang sowie ein Teil der Stadt."
+/>
