659897d423789ec701671b7a826042fa0622e500
Niklas Egger <egger@hdm-stuttgart.de>
2024-09-17 15:19:22 +0200

    add link to certificate course

--- src/lib/constants/certificateCourse.constant.ts
+++ src/lib/constants/certificateCourse.constant.ts
@@ -9,13 +9,13 @@ export const CERTIFICATE_COURSE: Project = {
 	targetGroup: ['Für Lehrende'],
 	summary:
 		'Dieser Zertifikatskurs befasst sich mit der Bedeutung der digitalen Barrierefreiheit in der Hochschulbildung. Er beleuchtet die demografischen Bedürfnisse, die rechtlichen Aspekte der inklusiven Bildung sowie die aktuellen Erfahrungen einiger Studierender mit Behinderungen in Universitätskursen. Er konzentriert sich auch auf praktische Tipps und Techniken, wie man digitale Lehrmaterialien und Plattformen besser barrierefrei gestalten kann. Zertifikat „Barrierefrei lehren“',
-	// link: {
-	// 	label: 'Zum Zertifikat: Barrierefrei lehren',
-	// 	url: 'https://mooc.hdm-stuttgart.de/course/view.php?id=43'
-	// },
-	showInDevelopmentInfo: true,
+	link: {
+		label: 'Zum Lernkurs',
+		url: 'https://mooc.hdm-stuttgart.de/course/view.php?id=43'
+	},
+	showInDevelopmentInfo: false,
 	partners: ['Hochschule der Medien Stuttgart'],
-	// info: 'Damit Sie Zugang zu den Inhalten des Kurses erhalten, müssen Sie sich vorab anmelden. Dazu können Sie ein bestehendes Moodle-Konto nutzen oder ein kostenloses Konto anlegen. Nach dem erfolgreichen Login haben Sie Zugriff auf den Kurs.',
+	info: 'Damit Sie Zugang zu den Inhalten des Kurses erhalten, müssen Sie sich vorab anmelden. Dazu können Sie ein bestehendes Moodle-Konto nutzen oder ein kostenloses Konto anlegen. Nach dem erfolgreichen Login haben Sie Zugriff auf den Kurs.',
 	joinEvaluation: {
 		title: 'Feedback geben',
 		introduction:
