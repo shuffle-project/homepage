c6c34ae6716c6035b8549b21d45de12b780c8961
Niklas Egger <egger@hdm-stuttgart.de>
2024-10-16 14:20:04 +0200

    update project filter

--- src/lib/constants/filter.constant.ts
+++ src/lib/constants/filter.constant.ts
@@ -4,14 +4,17 @@ import { BLINDDATE } from './blindDate.constant';
 import { CERTIFICATE_COURSE } from './certificateCourse.constant';
 import { DIGITAL_ACCESSIBILITY_UNIVERSITY } from './digitalAccessibilityUniversity.constant';
 import { IAAP_TOOLKITS } from './iaapToolkits.constant';
+import { ILIAS_COURSE } from './iliasCourse.constant';
 import { MATURITY_MODEL } from './maturityModel.constant';
 import { MELVIN } from './melvin.constant';
+import { MOOC_COURSES } from './moocCourses.constant';
 
 export const CONSULTATION_PROJECTS = [MATURITY_MODEL];
-export const AWARENESS_PROJECTS = [BLINDDATE];
+export const AWARENESS_PROJECTS = [BLINDDATE, MOOC_COURSES];
 export const TEACHING_PROJECTS = [
 	ACCESSIBLE_MATERIAL_PACK,
 	ACCESSIBLE_MOODLE_COURSE,
+	ILIAS_COURSE,
 	BLINDDATE,
 	MELVIN
 ];
