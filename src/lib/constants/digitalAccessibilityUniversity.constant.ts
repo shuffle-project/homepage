0ffcebc159b3bf0f628acacf30f89119e1ca0336
Niklas Egger <egger@hdm-stuttgart.de>
2024-11-20 10:23:18 +0100

    add missing webinar to project subpage

--- src/lib/constants/digitalAccessibilityUniversity.constant.ts
+++ src/lib/constants/digitalAccessibilityUniversity.constant.ts
@@ -1,7 +1,8 @@
 import type { Project } from '$lib/interfaces/project.interface';
 
 export const DIGITAL_ACCESSIBILITY_UNIVERSITY: Project = {
-	id: 'digitale-barrierefreiheit-an-hochschulen',
+	// id: 'digitale-barrierefreiheit-an-hochschulen',
+	id: 'handlungsfelder-digitale-inklusion',
 	// title: 'Digitale Barrierefreiheit an Hochschulen',
 	title: 'Handlungsfelder digitale Inklusion',
 	category: 'Moodle-Kurs',
@@ -12,6 +13,7 @@ export const DIGITAL_ACCESSIBILITY_UNIVERSITY: Project = {
 		'Die Materialien die in dem Kurs bereitgestellt sind, richten sich an alle Akteure an Hochschulen, die ihr Verständnis für das Thema der digitalen Barrierefreiheit vertiefen und aktiv dazu beitragen möchten, ihre Institutionen systematisch zugänglicher zu gestalten. Die Kursmaterialien bieten nicht nur zentrale Informationen zu verschiedenen Themen der digitalen Barrierefreiheit, sondern auch konkrete Handlungsempfehlungen, die Sie in Ihrer täglichen Arbeit anwenden können.',
 	showInDevelopmentInfo: true,
 	partners: ['Universität Bielefeld', 'Hochschule der Medien Stuttgart'],
+	webinar: 'reifegradmodell',
 	joinEvaluation: {
 		title: 'Melden Sie sich bei uns!',
 		introduction:
